7d6f42d5d553f536f56aa58d299b149eb92b25b2
ToJluK-V <57329726+ToJluK-V@users.noreply.github.com>
2021-11-12 14:47:05 +0300

    Commit_icon_vk_inst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Maxim - v4.6.0
-* Template URL: https://bootstrapmade.com/maxim-free-onepage-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
